194522649c61f26bc0f96c1b1d736d960f901e22
EAirPeter <VioletCrestfall@hotmail.com>
2015-01-06 18:13:21 +0800

    ...

--- src/main/java/cn/academy/ability/teleporter/client/render/RenderTpTarget.java
+++ src/main/java/cn/academy/ability/teleporter/client/render/RenderTpTarget.java
@@ -3,6 +3,7 @@ package cn.academy.ability.teleporter.client.render;
 import org.lwjgl.opengl.GL11;
 
 import cn.academy.ability.teleporter.entity.EntityTpTarget;
+import cn.academy.core.AcademyCraftMod;
 import cn.academy.core.proxy.ACClientProps;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.entity.Render;
@@ -15,6 +16,7 @@ public class RenderTpTarget extends Render {
 	@Override
 	public void doRender(Entity entity, double x, double y, double z,
 			float var8, float var9) {
+		AcademyCraftMod.log.info("render");
 		GL11.glPushMatrix();
 		GL11.glTranslated(x, y, z);
 		RenderBlocks.getInstance().renderBlockAsItem(Blocks.tnt, 0, 1.0F);
